ebb2bdb7a39f9633c0b2447ef756917b879c70a9
Marija K <kccmrj@outlook.com>
2022-04-19 17:40:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 
-function solution(string) {
+
+
+function expandedForm(num) {
+  let str = num.toString();
+  let len = str.length;
+  let output = "";
+  for (let i = 0; i < len; ++i) {
+    let char = str.charAt(i);
+    if (char != 0) {
+      output = output + `${parseInt(char) * Math.pow(10, len - i - 1)} + `;
+    }
+  }
+  console.log(output.slice(0, output.length - 3)); //pay attention to all empty spaces at the end
+}
+
+//expandedForm(324567)  // SOLVED
+
+
+function breakCamelCase(string) {
   let len = string.length;
   let char = "";
   for (let i = 0; i < len; i++) {
@@ -12,18 +30,4 @@ function solution(string) {
   console.log(string.toString());
   console.log(typeof string);
 }
-
-function expandedForm(num) {
-  let str = num.toString();
-  let len = str.length;
-  let output = "";
-  for (let i = 0; i < len; ++i) {
-    let char = str.charAt(i);
-    if (char != 0) {
-      output = output + `${parseInt(char) * Math.pow(10, len - i - 1)} + `;
-    }
-  }
-  console.log(output.slice(0, output.length - 3)); //pay attention to empty spaces at the end
-}
-
-expandedForm(324567)
+breakCamelCase('keechKeechKeech')
