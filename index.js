11d7378a450309f1f01ffa4a7869ebafbeb8b477
Marija K <kccmrj@outlook.com>
2022-04-19 21:53:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,4 +30,33 @@ function breakCamelCase(string) {
   console.log(string.toString());
   console.log(typeof string);
 }
-breakCamelCase('keechKeechKeech')
+//breakCamelCase('keechKeechKeech')
+
+function convertToRoman(num) {
+  let str = num.toString();
+  let len = str.length;
+  let output = [];
+  let output2 = [];
+  for (let i = 0; i < len; ++i) {
+    let char = str.charAt(i);
+    let roman = parseInt(char) * Math.pow(10, len - i - 1);
+    for(let j = 0; j < parseInt(char); ++j){
+     if (char != 0) {
+      output.push(roman/char);
+    }
+   }
+  }
+  console.log(output);
+
+//positive integer
+//max 3 same letters in a row
+// I          1
+// V          5
+// X          10
+// L          50
+// C          100
+// D          500
+// M          1000
+}
+
+convertToRoman(2022)
