ea4d06a61e96b227eba7f6090808e9d40c245b3d
Marija K <kccmrj@outlook.com>
2022-04-20 00:22:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,13 +35,25 @@ function breakCamelCase(string) {
 function convertToRoman(num) {
   let str = num.toString();
   let len = str.length;
-  let output = [];
+  let output = "";
   for (let i = 0; i < len; ++i) {
     let char = str.charAt(i);
     let roman = parseInt(char) * Math.pow(10, len - i - 1);
     for(let j = 0; j < parseInt(char); ++j){
      if (char != 0) {
-      output.push(roman/char);
+      output = output
+        .concat(roman / char)
+        .replace("1000", "M")
+        .replace("CCCCCCCCC", "CM")
+        .replace("LXXXX", "XC")
+        .replace("100", "C")
+        .replace("XXXXXX", "LX")
+        .replace("XXXXX", "L")
+        .replace("XXXX", "XL")
+        .replace("10", "X")
+        .replace("11111", "V")
+        .replace("1", "I");
+                     
     }
    }
   }
@@ -49,13 +61,13 @@ function convertToRoman(num) {
 
 //positive integer
 //max 3 same letters in a row
-// I     1       IV   4   (1 + 5)   DC  600   (500 + 100)
-// V     5       VI   6   (5 + 1)   CM  900   (100 + 1000)
-// X     10      IX   9   (1 + 10)
-// L     50      XL   40  (10 + 50)
-// C     100     LX   60  (50 + 10)
-// D     500     XC   90  (10 + 100)
+// I     1       IV   4   (1 + 5)        DC  600   (500 + 100)
+// V     5       VI   6   (5 + 1)        CM  900   (100 + 1000)
+// X     10      IX   9   (1 + 10)       III   111
+// L     50      XL   40  (10 + 50)      XXX   101010
+// C     100     LX   60  (50 + 10)      CCC   100100100
+// D     500     XC   90  (10 + 100)     MMM   100010001000
 // M     1000    CD   400 (100 + 500)
 }
 
-convertToRoman(2099)
+convertToRoman(1990)
