18ce5421e94a7afb0fea29bb4a3ae512f5b9846b
Marija K <kccmrj@outlook.com>
2022-07-14 23:55:33 +0200

    practice

--- code02.js
+++ code02.js
@@ -12,29 +12,103 @@
   //repeats it until array[0] === 1 
 
 
-  function fannkuchRedux(arr) {
-      if (arr[0] !== 1) {
-      let n = arr[0];    
-      let arrX = arr.slice(0, n);
-      let arrY = arr.slice(n)
-      let arrXrev = [];
-      for (let i = 1; i <= n; ++i){ 
-          arrXrev.push(arrX[n-i]) // subtract i (that increments by one with every cycle) from n until i = n -- until arrX[n-n] = arrX[0] -- until the first element of arrX is pushed into the last spot in arrXrev
+  // function fannkuchRedux(arr) {
+  //     if (arr[0] !== 1) {
+  //     let n = arr[0];    
+  //     let arrX = arr.slice(0, n);
+  //     let arrY = arr.slice(n)
+  //     let arrXrev = [];
+  //     for (let i = 1; i <= n; ++i){ 
+  //         arrXrev.push(arrX[n-i]) // subtract i (that increments by one with every cycle) from n until i = n -- until arrX[n-n] = arrX[0] -- until the first element of arrX is pushed into the last spot in arrXrev
+  //     }
+  //     //console.log(arrY);
+  //     //console.log(arrXrev);
+  //     let newArr = [...arrXrev, ...arrY];
+  //     console.log(newArr)
+  //     arr = newArr;
+  //     fannkuchRedux(arr)
+  //   }else{
+  //       console.log('arr[0] is 1 now')
+  //   }
+  // }
+
+  // //fannkuchRedux([4,2,1,5,3])
+
+  // for(let i = 1; i<5; ++i){
+  //   document[`value${i}`] = i;
+  //   console.log
+  // }
+
+
+  // function toRange(arr) {
+  //   let newArr = [];
+  //   let len = arr.length;
+  //   for (let i = 0; i < len-1; i=i+2){
+  //     if(arr[i+1] - arr[i] >= 2){
+  //       newArr.push(`${arr[i]}-${arr[i+1]}`)
+  //     } else {
+  //       newArr.push(arr[i], arr[i+1])
+  //     }
+  //   }
+  //   console.log(newArr.join(','))
+  // }
+
+  // toRange([
+  //   -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20,
+  // ]);
+
+  let myArr = [
+    -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20,
+  ];
+
+  function oneRange(arr) { 
+    let newArr = [];
+    let len = arr.length;
+    for(let i = 0; i < len; ++i){
+      if(arr[i+1] != arr[i]+1){ 
+        if(arr[i+1] != undefined){
+        newArr.push(arr[i], arr[i+1])
+        }else{
+          newArr.push(arr[i])
+        }
       }
-      //console.log(arrY);
-      //console.log(arrXrev);
-      let newArr = [...arrXrev, ...arrY];
-      console.log(newArr)
-      arr = newArr;
-      fannkuchRedux(arr)
-    }else{
-        console.log('arr[0] is 1 now')
     }
+    console.log(newArr)
   }
 
-  //fannkuchRedux([4,2,1,5,3])
+  //oneRange(myArr)
 
-  for(let i = 1; i<5; ++i){
-    document[`value${i}`] = i;
-    console.log
+const isSubset = (array1, array2) =>
+  array2.every((element) => array1.includes(element));
+
+
+  function theRanger(arr) {
+    let len = arr.length;
+    let rangePlus = [];
+    let validRanges = [];
+    for (let i = 0; i < len-1; ++i){
+      if (arr[i]+1 === arr[i+1]){
+       let x = 1; 
+       let range = [arr[i]];  
+        while ( arr[i+x] === arr[i] + x ) {
+          range.push(arr[i+x])
+          ++x;    
+        }
+        if (range.length >= 3) {
+        rangePlus.push(range);  
+       } 
+      }
+     
+    }
+    console.log(rangePlus)
+    rangePlus.forEach(ran => {
+      for(let j=rangePlus.indexOf(ran)+1; j<rangePlus.length; ++j){
+       if (isSubset(ran, rangePlus[j])){
+        validRanges.push(ran)
+       }
+        }
+    })
+    console.log(validRanges)
   }
+
+  theRanger(myArr)
