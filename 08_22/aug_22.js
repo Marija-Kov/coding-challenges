643d77397d37e398a334730411ea4ca1c7e90998
Marija K <kccmrj@outlook.com>
2022-08-04 23:43:03 +0200

    Create aug_22.js

---
+++ 08_22/aug_22.js
@@ -0,0 +1,13 @@
+//will take any string, convert it to camelCase + the first letter must be capitalized
+//the result must have # as the first char
+//all whitespace must be removed
+//if the final result is longer than 140 chars, must return false // including or excluding the # ?
+//if the string is "", also returns false // what about "   "  ?
+// what about numerical chars? how to handle other non-letter chars ? What about special letter chars?
+//1. all first letters to uppercase
+//2. trim whitespace
+//3. add hashtag
+
+function generateHashtag(str) {
+
+}
