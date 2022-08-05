2957c7e1a513c199446f3c189c5e5f8a8222fa9b
Marija K <kccmrj@outlook.com>
2022-08-05 19:38:41 +0200

    practice algos

--- 08_22/aug_22.js
+++ 08_22/aug_22.js
@@ -1,3 +1,4 @@
+//1.
 //will take any string, convert it to camelCase + the first letter must be capitalized
 //the result must have # as the first char
 //all whitespace must be removed
@@ -8,6 +9,50 @@
 //2. trim whitespace
 //3. add hashtag
 
-function generateHashtag(str) {
+// function generateHashtag(str) {
+//  if (str==false){
+//      return false
+//  };
+//  let words = str.split(' ').filter(e => !e===false);
+//  let capped = [];
+//  for (let i = 0; i < words.length; ++i){
+//     capped.push(words[i].charAt(0).toUpperCase() + words[i].slice(1)); // cat --> "C" + "at" --> Cat
+//  };
+//  if (capped.join('').length + 1 > 140) {
+//      return false
+//  } else {
+//  return `#${capped.join('')}`
+//  };
+// }
 
-}
+
+// console.log(generateHashtag("            Hello there thanks              for trying my Kata"));
+// console.log(generateHashtag(""));
+
+
+// 2.
+// given an object of recipe ingredients and an object of available ingredients
+// calculate max number of cakes that can be made
+// ingredient amounts are just numbers
+
+
+// function cakes(recipe, available) {
+//   let rec = Object.keys(recipe); 
+//   let ava = Object.keys(available);
+//   let chancesOfCake = [];
+//   for (let i=0; i < rec.length; ++i) {
+//       if (ava.includes(rec[i])) {
+//         chancesOfCake.push(Math.floor(available[rec[i]] / recipe[rec[i]])) // for every available recipe ingredient, the cakeable amount is calculated and pushed into an array.
+//         } else {
+//             return 0 // if a single recipe ingredient is unavailable, no cake can be made.
+//         }
+//   }
+
+//   return Math.min(...chancesOfCake) // if all of the recipe ingredients are available, the least cakeable one denotes the total cakeability.
+ 
+// }
+
+
+
+// console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}));
+// console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}));
