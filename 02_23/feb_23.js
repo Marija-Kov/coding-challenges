688648fcacc561230591cee47450dedf685f9991
Marija K <kccmrj@outlook.com>
2023-02-17 11:32:01 +0100

    practice - arrays

--- 02_23/feb_23.js
+++ 02_23/feb_23.js
@@ -30,4 +30,55 @@ function findNextSquare(sq) {
 }
 
 // console.log(findNextSquare(121))
-// console.log(findNextSquare(114));
+// console.log(findNextSquare(114));// console.log(findNextSquare(114));
+
+// In other words: Replace the number in the array with the number corresponding to what its index+1 would be if the array was sorted in the descending order
+// Input: array of random positive integers, no invalid input
+// Output: array of positive integers
+// Solution: 
+// 1. Sort the input array in the descending order (this would be a new array named 'desc')
+// 2. create a new empty array (named 'ranked') 
+// 3. Iterate through the input array; for each value of the input, get its index in the desc array and push it into ranked
+// 4. return ranked
+
+// a)
+// function rankings(arr){
+//     let desc = arr.map((a)=>a).sort((a,b)=>a-b).reverse();
+//     return arr.map(a=>desc.indexOf(a)+1) 
+// }
+
+// b) 30+% faster than a):
+// function rankings(arr) {
+//   let len = arr.length;
+//   let desc = arr
+//     .map((a) => a)
+//     .sort((a, b) => a - b)
+//     .reverse();
+//   let ranked = [];
+//   for (let i = 0; i < len; ++i) {
+//     ranked.push(desc.indexOf(arr[i])+1);
+//   }
+//   return ranked;
+// }
+
+// c) 40+% faster than b):
+function rankings(arr) {
+  let len = arr.length;
+  let arrCopy = [];
+  for(let i=0; i<len; ++i){
+    arrCopy.push(arr[i])
+  }
+  let desc = arrCopy
+    .sort((a, b) => a - b)
+    .reverse();
+  let ranked = [];
+  for (let i = 0; i < len; ++i) {
+    ranked.push(desc.indexOf(arr[i]) + 1);
+  }
+  return ranked;
+}
+
+// interesting solution (the way positions of numbers were obtained without using extra space): https://www.codewars.com/kata/reviews/58e19f807bf60324f30002ca/groups/63c96e55424ee40001084a26
+
+// console.log(rankings([10, 20, 40, 50, 30]));
+// console.log(rankings([22, 33, 18, 9, 110, 4, 1, 88, 6, 50]));
